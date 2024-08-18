264bc59269a8c5ccc77522d815f70e648106f3af
igortasic123 <63684263+igortasic123@users.noreply.github.com>
2024-08-18 10:46:27 -0400

    website

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,16 @@ const font = Space_Grotesk({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Bird | Design & Development Studio",
   description: "Grow your business with Bird, a design & development studio.",
+  openGraph: {
+    images: [
+      {
+        url: 'https://www.birdagency.io/logo/logo.webp',
+        width: 1200,
+        height: 630,
+        alt: 'Bird Logo',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
